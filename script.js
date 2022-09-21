9601bd38e9e12b6289fc273643042e6897ad8a38
Ember-M <ember.maes@gmail.com>
2022-09-21 00:22:59 -0700

    new file:   Saturday Bright.woff 	modified:   index.html 	modified:   script.js 	new file:   simplicity.ttf 	modified:   style.css     Add book form in sidebar.     Add backend function to create book based off form values.     Add backend function to iterate through and display the book Array on card divs.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let libraryArray = [];
+let bookCount = 0;
+
+let addBookButton = document.getElementById("addBook");
+let titleField = document.getElementById("title");
+let authorField = document.getElementById('author');
+let pagesField = document.getElementById('pages');
+let readField = document.getElementById('read');
+
+
+
 function BookConstructor(title,author,pages,read){
     this.title = title,
     this.author = author,
@@ -9,3 +19,48 @@ function BookConstructor(title,author,pages,read){
 function addBookToLibrary(book){
     libraryArray.push(book)
 }
+
+function createBook(){
+    let title = titleField.value;
+    let author = authorField.value;
+    let pages = pagesField.value;
+    let read = readField.checked;
+    book = new BookConstructor(title, author, pages, read)
+}
+
+function displayBookArray(array){
+    const bookCard = document.createElement('div');
+    let titleDisplay = document.createElement('p');
+    let authorDisplay = document.createElement('p');
+    let pagesDisplay = document.createElement('p');
+    let readDisplay = document.createElement('p')
+    array.forEach(element => {
+        titleDisplay.innerHTML = `Book Title: <br> ${element.title}`;
+        authorDisplay.innerHTML = `Book Author:<br> ${element.author}`;
+        pagesDisplay.innerHTML = `Number of pages: <br> ${element.pages}`;
+        if (element.read === true){
+            readDisplay.innerHTML = `You have read this book`;
+        }
+        else{
+            readDisplay.innerHTML = `You have not read this book`;
+        }
+        bookCard.appendChild(titleDisplay);
+        bookCard.appendChild(authorDisplay);
+        bookCard.appendChild(pagesDisplay);
+        bookCard.appendChild(readDisplay);
+        document.getElementById('libraryContainer').appendChild(bookCard);
+    });
+}
+
+addBookButton.addEventListener('click', ()=> {
+    if (titleField.value == ''||authorField.value == ''||pagesField.value ==''){
+        return
+    }
+    else{
+    createBook();
+    addBookToLibrary(book);
+    displayBookArray(libraryArray);
+    console.log(book.title);
+    console.log(libraryArray)
+    }
+})
